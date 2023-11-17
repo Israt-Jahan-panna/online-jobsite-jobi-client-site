ac59ddd4ed614a5935fe3deb7622af5348ca3a94
Israt Jahan <isratjahanpanna87@gmail.com>
2023-11-17 14:55:03 +0600

    configure firebase authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFVT-jSz-DrhAj3BxusgB3hT1n-xizTg4",
+  authDomain: "jobi-clientsite.firebaseapp.com",
+  projectId: "jobi-clientsite",
+  storageBucket: "jobi-clientsite.appspot.com",
+  messagingSenderId: "326545897468",
+  appId: "1:326545897468:web:69ec1cb77d83875014e479"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
