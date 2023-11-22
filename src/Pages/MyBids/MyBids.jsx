2f99b634c5cf4fd8c20a8aa24affe15a281d768f
Israt Jahan <isratjahanpanna87@gmail.com>
2023-11-23 02:40:53 +0600

    update Error page

--- src/Pages/MyBids/MyBids.jsx
+++ src/Pages/MyBids/MyBids.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardTab from '../../Compunents/cardtab/cardTab';
 
 const MyBids = () => {
+    const [myBids , setMyBids] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4100/jobs')
+    .then(res => res.json())
+    .then(data => setMyBids(data))
+   
+  });
     return (
         <div>
-            <CardTab></CardTab>
+            {
+        myBids.map(jobs=> <CardTab key={jobs.id} jobs={jobs}></CardTab>)
+         }
         </div>
     );
 };
