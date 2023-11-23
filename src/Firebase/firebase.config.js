6bbfc2511a031251d71fd3a3f349f4dbce6aadb3
Israt Jahan <isratjahanpanna87@gmail.com>
2023-11-23 21:22:22 +0600

    modify keys hied

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFVT-jSz-DrhAj3BxusgB3hT1n-xizTg4",
-  authDomain: "jobi-clientsite.firebaseapp.com",
-  projectId: "jobi-clientsite",
-  storageBucket: "jobi-clientsite.appspot.com",
-  messagingSenderId: "326545897468",
-  appId: "1:326545897468:web:69ec1cb77d83875014e479"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
